70ee5a9c3ad4f5114baf485ec45b1940df3a1686
ktapia576 <ktapia576@hotmail.com>
2021-01-30 18:16:11 -0500

    Add device list to HandPresence script and log if primary button pressed

---
+++ Assets/HandPresence.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    private InputDevice targetDevice;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+        InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        foreach (var device in devices)
+        {
+            Debug.Log("name: " + device.name + " characteristic: "+ device.characteristics);
+        }
+
+        // Make sure there is at least one device connected
+        if (devices.Count > 0) 
+        {
+            targetDevice = devices[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        // PrimaryButtonValue = true if pressed
+        if (primaryButtonValue)
+            Debug.Log("Primary Button was pressed!");
+    }
+}
