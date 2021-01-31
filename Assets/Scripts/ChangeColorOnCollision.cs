152474082bc716afdf808b5e9912b82569cc4d62
ktapia576 <ktapia576@hotmail.com>
2021-01-31 18:54:53 -0500

    Add Trigger Event script for cube and add transaprency to white material

--- Assets/Scripts/ChangeColorOnCollision.cs
+++ Assets/Scripts/ChangeColorOnCollision.cs
@@ -7,12 +7,12 @@ public class ChangeColorOnCollision : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         Color randomlySelectedColor = GetRandomColor();
-        GetComponent<Renderer>().material.color = randomlySelectedColor;
+        GetComponent<Renderer>().material.color = randomlySelectedColor; // Get the renderer of this object and change the material color
     }
 
     private Color GetRandomColor()
     {
-        return new Color(
+        return new Color(   // Creates a new Color from 0-1 for rgb (on slider scale)
             Random.Range(0f, 1f),
             Random.Range(0f, 1f),
             Random.Range(0f, 1f));
---
+++ Assets/Scripts/ChangeColorOnTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) // The collider of the other object that hits this trigger event or trigger hits
+    {
+        Color randomlySelectedColor = GetRandomColorWithAlpha();
+        GetComponent<Renderer>().material.color = randomlySelectedColor; // Get the renderer of this object and change the material color
+    }
+
+    private Color GetRandomColorWithAlpha()
+    {
+        return new Color(   // Creates a new Color from 0-1 for rgb (on slider scale) | Alpha sets transparency
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            0.9f);
+    }
+}
