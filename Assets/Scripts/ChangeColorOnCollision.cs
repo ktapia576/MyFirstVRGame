a7f064eb311e490d113cb7d47bf76ef65d2df0c3
ktapia576 <ktapia576@hotmail.com>
2021-01-31 18:05:09 -0500

    Add on collision change color script to ball

---
+++ Assets/Scripts/ChangeColorOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Color randomlySelectedColor = GetRandomColor();
+        GetComponent<Renderer>().material.color = randomlySelectedColor;
+    }
+
+    private Color GetRandomColor()
+    {
+        return new Color(
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f),
+            Random.Range(0f, 1f));
+    }
+}
