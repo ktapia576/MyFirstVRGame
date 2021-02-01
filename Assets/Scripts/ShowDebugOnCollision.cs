aa52024b543aa318b419ee573e781c9ac6a82d09
ktapia576 <ktapia576@hotmail.com>
2021-01-31 19:26:39 -0500

    Add Debug Drawline for getting contact points of collisions

---
+++ Assets/Scripts/ShowDebugOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDebugOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Impacted at "+ collision.contacts[0].point);
+
+        Debug.DrawRay(
+            collision.contacts[0].point,   // the point where the collision happened 
+            collision.contacts[0].normal,   // the direction that point is facing is the normal
+            Color.red,
+            1f);
+    }
+}
